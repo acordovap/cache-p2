59152f917d5b298d8b6f1c9e05d0dbf404e09ee0
Alan Córdova Posadas <acordovap@102mcc13.inge.itam.mx>
2020-03-10 00:55:56 -0600

    script and output for report

--- report.sh
+++ report.sh
@@ -37,4 +37,43 @@ do
 done
 
 #2.4
-echo "++++++++++++++++++++++++++++++++ 2.4 "
+echo "++++++++++++++++++++++++++++++++ 2.4a "
+max=64
+for f in trazas/cc.trace trazas/spice.trace trazas/tex.trace
+do
+  echo "-------------------------------- $f --------------------------------"
+  for p in wb wt # policy
+  do
+    for i in 8192 16384 32768 # cache size
+    do
+      for j in 64 128 # block size
+      do
+        for k in 2 4 # associativity
+        do
+          ./sim -is $i -ds $i -bs $j -a $k -$p -nw $f
+        done
+      done
+    done
+  done
+done
+
+echo "++++++++++++++++++++++++++++++++ 2.4b "
+max=64
+for f in trazas/cc.trace trazas/spice.trace trazas/tex.trace
+do
+  echo "-------------------------------- $f --------------------------------"
+  for p in wa nw # policy
+  do
+    for i in 8192 16384 32768 # cache size
+    do
+      for j in 64 128 # block size
+      do
+        for k in 2 4 # associativity
+        do
+          ./sim -is $i -ds $i -bs $j -a $k -wb -$p $f
+          ./sim -is $i -ds $i -bs $j -a $k -wb -$p $f
+        done
+      done
+    done
+  done
+done
